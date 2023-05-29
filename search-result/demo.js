9b13777b72a674a4afa56a58e46bfbca6e415472
snellers <1669156+snellers@users.noreply.github.com>
2023-05-29 19:36:52 +0000

    license

--- search-result/demo.js
+++ search-result/demo.js
@@ -6,27 +6,27 @@ import {startFlow, desktopConfig} from 'lighthouse';
 const {getDocument, queries} = pptrTestingLibrary;
 
 async function search(page, elementPattern, searchTerm) {
-        const $document = await getDocument(page);
-        const $searchBox = await queries.getByPlaceholderText($document, elementPattern);
-        await $searchBox.type(searchTerm);
-        await Promise.all([
-                $searchBox.press('Enter'),
-                page.waitForNavigation({waitUntil: ['load', 'networkidle2']}),
-        ]);
+    const $document = await getDocument(page);
+    const $searchBox = await queries.getByPlaceholderText($document, elementPattern);
+    await $searchBox.type(searchTerm);
+    await Promise.all([
+            $searchBox.press('Enter'),
+            page.waitForNavigation({waitUntil: ['load', 'networkidle2']}),
+    ]);
 }
 
 async function click(page, elementPattern) {
-        await flow.navigate(async () => {
-                const $document = await getDocument(page);
-                const $links = await queries.getAllByText($document, elementPattern);
-                $links[0].click();
-        });
+    await flow.navigate(async () => {
+        const $document = await getDocument(page);
+        const $links = await queries.getAllByText($document, elementPattern);
+        $links[0].click();
+    });
 }
 
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 const flow = await startFlow(page, {
-     config: desktopConfig
+    config: desktopConfig
 });
 
 console.log('Opening site...');
